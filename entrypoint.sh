d15a319485dbad8ef4a76d6df8745021c5de176e
a-was <adam.wasilewski@unix.com.pl>
2020-11-26 11:22:32 +0100

    Update entrypoint.sh
    Add -e to echo
    Add "" and {}
    Change ssh_key.txt to key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-echo $INPUT_KEY > ssh_key.txt
-chmod 600 ssh_key.txt
+echo -e "${INPUT_KEY}" > key
+chmod 600 key
 
-scp -i ssh_key.txt \
+scp -v \
+    -i key \
     -o StrictHostKeyChecking=no \
-    -P $INPUT_PORT \
-    -r $INPUT_REPO_DIR \
-    "$INPUT_USER"@"$INPUT_HOST":"$INPUT_REMOTE_DIR"
+    -P "${INPUT_PORT}" \
+    -r "${INPUT_REPO_DIR}" \
+    "${INPUT_USER}"@"${INPUT_HOST}":"${INPUT_REMOTE_DIR}"
 
-shred -n 5 -u ssh_key.txt
+# shred and delete key
+shred -n 5 -u key
